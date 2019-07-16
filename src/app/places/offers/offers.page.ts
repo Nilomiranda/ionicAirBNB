ce79615f7204631b4b4e9bf3098ef0bad03c9942
Nilomiranda <nilomiranda3@gmail.com>
2019-07-16 08:56:07 -0300

    Adding some sick slidings

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../places.model';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-offers',
@@ -10,9 +12,14 @@ import { Place } from '../places.model';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private places: PlacesService) {}
+  constructor(private places: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.places.places;
+  }
+
+  onUpdate(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
   }
 }
